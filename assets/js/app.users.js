7a89e588c6229892d6d1db28ab2a4d1eaef117f9
Guilherme Silveira da Silva <guissilveira@gmail.com>
2016-10-13 18:18:18 -0300

    Create actions for PlanDays.

--- assets/js/app.users.js
+++ assets/js/app.users.js
@@ -56,9 +56,43 @@ Application.Controller.Users = (function($) {
             $("#create-form").show();
         });
 
+        // MANAGE PLANS action button
+        $("body").on("click", ".action-manage-plans", function() {
+
+            var id = $(this).data("id");
+
+            $.ajax({
+                url: Application.vars.api_url + 'users/' + id + '/'
+            })
+            .done(function(data) {
+                
+                $("#manage-plans-form form").attr("action", Application.vars.api_url + 'users/' + id + '/plans/');
+                $("#manage-plans-form input[name=firstname]").val(data.user.firstname);
+
+                // Set all checkboxes to unchecked
+                $("#manage-plans-form .plans input[type=checkbox]").each(function() {
+
+                    $(this).prop('checked', false);
+                });
+
+                // Set user plans checboxes to checked
+                data.user.plans.forEach(function(plan) {
+
+                    $("#manage-plans-form .plans input[data-plan_id=" + plan.id + "]").prop('checked', true);
+                });
+
+                $("#list").hide();
+                $("#manage-plans-form").show();
+            })
+            .fail(function( jqXHR, textStatus ) {
+                alert( "Request failed: " + textStatus );
+            });
+        });
+
         // Hide edit and create forms
         $("#edit-form").hide();
         $("#create-form").hide();
+        $("#manage-plans-form").hide();
 
         // Apply ajaxForm function to make ajax request by default
         $('form').ajaxForm({
@@ -76,7 +110,22 @@ Application.Controller.Users = (function($) {
 
             data.users.forEach(function(item) {
                 
-                $("table.grid tbody").append("<tr><th scope=\"row\">" + item.id + "</th><td>" + item.firstname + "</td><td>" + item.email + "</td><td><button type=\"button\" class=\"btn btn-default action-edit\" data-id=\"" + item.id + "\">Edit</button> <button type=\"button\" class=\"btn btn-default action-delete\" data-id=\"" + item.id + "\">Delete</button></td></tr>");
+                $("table.grid tbody").append("<tr><th scope=\"row\">" + item.id + "</th><td>" + item.firstname + "</td><td>" + item.email + "</td><td>" + item.plans.length + " <button type=\"button\" class=\"btn btn-default action-manage-plans\" data-id=\"" + item.id + "\">Manage</button></td><td><button type=\"button\" class=\"btn btn-default action-edit\" data-id=\"" + item.id + "\">Edit</button> <button type=\"button\" class=\"btn btn-default action-delete\" data-id=\"" + item.id + "\">Delete</button></td></tr>");
+            });
+        })
+        .fail(function( jqXHR, textStatus ) {
+            alert( "Request failed: " + textStatus );
+        });
+
+        // Load plans data
+        $.ajax({
+            url: Application.vars.api_url + 'plans/'
+        })
+        .done(function(data) {
+
+            data.plans.forEach(function(item) {
+                
+                $("#manage-plans-form .plans").append("<div class=\"checkbox\"><label><input type=\"checkbox\" name=\"plans[]\" value=\"" + item.id + "\" data-plan_id=\"" + item.id + "\"> " + item.plan_name + "</label></div>");
             });
         })
         .fail(function( jqXHR, textStatus ) {
